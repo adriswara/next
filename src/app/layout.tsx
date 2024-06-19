3a4c11d2f0244afa5699eeb5fa67aa707072333b
adriswara <chevellerz454@yahoo.com>
2024-06-19 15:00:10 +0700

    complete header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Source_Sans_3 } from "next/font/google";
+
 import "./globals.css";
+import Image from "next/image";
+
+
 const inter = Inter({ subsets: ["latin"] });
+const sourceSans = Source_Sans_3({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jonas",
@@ -13,15 +18,46 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const imageHeader = {
+    marginLeft: 64,
+  }
+  const headerStyle = {
+    display: "grid",
+    gridTemplateColumns: "auto auto auto auto auto auto auto",
+  }
+  const headerContent = {
+    marginTop: 15,
+
+  }
+  const loginButton = {
+    backgroundColor: "#A4BB8D",
+    borderRadius: 30,
+    width: 144,
+    height: 30
+  }
   return (
     <html lang="en">
-      <section>
-        <nav>
-          <div><button></button></div>
-        </nav>
-          {children}
-      </section>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <section>
+          <nav style={headerStyle}>
+            <div style={imageHeader}>
+              <Image
+                src="/header.png"
+                width={230}
+                height={64}
+                alt="Picture of the author"
+              />
+            </div>
+            <div className={sourceSans.className} style={headerContent}><button>Product</button></div>
+            <div className={sourceSans.className} style={headerContent}><button>Showcase</button></div>
+            <div className={sourceSans.className} style={headerContent}><button>Voucher/Reward</button></div>
+            <div className={sourceSans.className} style={headerContent}><button>Profile</button></div>
+            <div className={sourceSans.className} style={headerContent}><button>About</button></div>
+            <div className={sourceSans.className} style={headerContent}><button style={loginButton}>Login/Signup</button></div>
+          </nav>
+        </section>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas illo debitis libero rem omnis doloremque quidem ex, quaerat, quis consequuntur ipsum esse ipsa repudiandae veritatis odit quo, quod at reprehenderit?</h1>
-
-     
-    </div>
   );
 }
 
