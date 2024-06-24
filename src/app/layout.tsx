6426e919d1aa8530de2a38e332d2e22e95cfbcc3
adriswara <chevellerz454@yahoo.com>
2024-06-24 10:53:44 +0700

    navbar done, sort of

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,21 +20,50 @@ export default function RootLayout({
 }>) {
   const imageHeader = {
     marginLeft: 64,
+    paddingTop: 16
   }
   const headerStyle = {
     display: "grid",
     gridTemplateColumns: "auto auto auto auto auto auto auto",
   }
   const headerContent = {
-    marginTop: 15,
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginLeft: 90,
+    marginRight: 80
+  }
+
+  const photoStudio = {
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginLeft: 350,
+    marginRight: -160
+  }
+
+  const photoPrint ={
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginLeft: 90,
+    marginRight: -160
+  }
+
+  const voucherButton ={
 
   }
-  const loginButton = {
-    backgroundColor: "#A4BB8D",
-    borderRadius: 30,
-    width: 144,
-    height: 30
+
+  const checkoutButton = {
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginRight: -24,
+    marginLeft: 82
   }
+  const profileButton = {
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginRight: -24,
+    marginLeft: -50
+  }
+
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -42,15 +71,14 @@ export default function RootLayout({
           <nav style={headerStyle}>
             <div style={imageHeader}>
               <a href="\">
-                <Image src="/header.png" width={230} height={64} alt="Picture of the author"/>
+                <Image src="/header.png" width={128} height={13.84} alt="Picture of the author" />
               </a>
             </div>
-            <div className={sourceSans.className} style={headerContent}><a href="\product">Product</a></div>
-            <div className={sourceSans.className} style={headerContent}><a href="\showcase">Showcase</a></div>
-            <div className={sourceSans.className} style={headerContent}><a href="\voucher">Voucher/Reward</a></div>
-            <div className={sourceSans.className} style={headerContent}><a href="\profile">Profile</a></div>
-            <div className={sourceSans.className} style={headerContent}><a href="\about">About</a></div>
-            <div className={sourceSans.className} style={headerContent}><a href="\login"><button style={loginButton}>Login/Signup</button></a></div>
+            <div className={sourceSans.className} style={photoStudio}><a href="\product">Photo Studio</a></div>
+            <div className={sourceSans.className} style={photoPrint}><a href="\showcase">Photo Print</a></div>
+            <div className={sourceSans.className} style={headerContent}><a href="\voucher">Frame</a></div>
+            <div className={sourceSans.className} style={checkoutButton}> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart "><circle cx="8" cy="21" r="1"></circle><circle cx="19" cy="21" r="1"></circle><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"></path></svg> </div>
+            <div className={sourceSans.className} style={profileButton}><a href="\login"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-user-round "><path d="M18 20a6 6 0 0 0-12 0"></path><circle cx="12" cy="10" r="4"></circle><circle cx="12" cy="12" r="10"></circle></svg> </a></div>
           </nav>
         </section>
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas illo debitis libero rem omnis doloremque quidem ex, quaerat, quis consequuntur ipsum esse ipsa repudiandae veritatis odit quo, quod at reprehenderit?</h1>
+      <h1>HomePage</h1>
   );
 }
 
