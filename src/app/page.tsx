42ca95d3eee1818406c03e81cb6b5454fd0ac5f3
adriswara <chevellerz454@yahoo.com>
2024-06-24 14:03:31 +0700

    re arranged navbar for profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  type logoutcolor = `rgb(${239},${77},${68})`
   const profileStyle = {
 
     display: "grid",
@@ -9,10 +10,15 @@ export default function Home() {
   }
 
   const navProfileCard = {
-    border: "2px solid lightGrey",
-    borderRadius: 5,
+    border: "1px solid lightGrey",
+    borderRadius: 10,
     marginLeft: 140,
     marginRight: 850
+  }
+  const navCardField = {
+    borderTop: "1px solid lightGrey",
+    paddingLeft: 0,
+    paddingRight: 0
   }
 
   const unselectedNavText = {
@@ -21,14 +27,23 @@ export default function Home() {
     paddingBottom: 5,
     paddingLeft: 10,
     paddingRight: "1 rem",
-    width :"100%"
+    width: "100%"
+  }
 
+  const logoutButton = {
+    color: "orange",
+    fontSize: "875 rem",
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 10,
+    paddingRight: "1 rem",
+    width: "100%"
   }
 
   const profileCard = {
-    border: "2px solid lightGrey",
+    border: "0.1px solid lightGrey",
     borderRadius: 5,
-    marginLeft: -825  ,
+    marginLeft: -825,
     marginRight: 145
   }
 
@@ -37,28 +52,35 @@ export default function Home() {
       <div style={navProfileCard}>
         <ul>
           <li><a href=""><p style={unselectedNavText}>Profile</p></a></li>
-          <li><a href=""><p style={unselectedNavText}>Change Password</p></a></li>
-          <li><a href=""><p style={unselectedNavText}>Transaction</p></a></li>
-          <li><a href=""><p style={unselectedNavText}>Logout</p></a></li>
+          <li style={navCardField}><a href=""><p style={unselectedNavText}>Change Password</p></a></li>
+          <li style={navCardField}><a href=""><p style={unselectedNavText}>Transaction</p></a></li>
+          <li style={navCardField}><a href=""><p style={unselectedNavText}>Voucher List</p></a></li>
+          <li style={navCardField}><a href=""><p style={unselectedNavText}>Voucher Redeem</p></a></li>
+          <li style={navCardField}><a href=""><p style={logoutButton}>Logout</p></a></li>
         </ul>
       </div>
       <table style={profileCard}>
-        <tr>
-          <th><p>Profile</p></th>
-          <th></th>
-        </tr>
-        <tr>
-          <td>Name</td>
-          <td>Name Field</td>
-        </tr>
-        <tr>
-          <td>Email</td>
-          <td>emailfield@mail.com</td>
-        </tr>
-        <tr>
-          <td>Phone</td>
-          <td>+62812345678</td>
-        </tr>
+        <thead>
+          <tr>
+            <th colSpan={1}><p>Profile</p></th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Name Field</td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>emailfield@mail.com</td>
+          </tr>
+          <tr>
+            <td>Phone</td>
+            <td>+62812345678</td>
+          </tr>
+
+        </tbody>
       </table>
     </div>
   );
