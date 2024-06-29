ea4aa1aa2369bca6571940aecf937962e662f463
adriswara <chevellerz454@yahoo.com>
2024-06-29 20:13:03 +0700

    by name read

--- golang/data-access/main.go
+++ golang/data-access/main.go
@@ -10,6 +10,14 @@ import (
 
 var db *sql.DB
 
+type Voucher struct {
+	ID          int64
+	description int64
+	discount    string
+	title       string
+	status      int64
+}
+
 func main() {
 	// Capture connection properties.
 	cfg := mysql.Config{
@@ -32,4 +40,33 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected!")
+
+	voucher, err := voucherByTitle("Photo Studio")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Voucher found: %v\n", voucher)
+}
+
+func voucherByTitle(name string) ([]Voucher, error) {
+	// An albums slice to hold data from returned rows.
+	var voucher []Voucher
+
+	rows, err := db.Query("SELECT * FROM voucher WHERE title = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("voucherByTitle %q: %v", name, err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Voucher
+		if err := rows.Scan(&alb.ID, &alb.title, &alb.description, &alb.status, &alb.discount); err != nil {
+			return nil, fmt.Errorf("voucherByTitle %q: %v", name, err)
+		}
+		voucher = append(voucher, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("voucherByTitle %q: %v", name, err)
+	}
+	return voucher, nil
 }
