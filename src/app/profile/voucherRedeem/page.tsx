66484c6a4d14c446a68a6105a1e9628c0618ec89
adriswara <chevellerz454@yahoo.com>
2024-07-03 10:25:08 +0700

    delete unused for redeem

--- src/app/profile/voucherRedeem/page.tsx
+++ src/app/profile/voucherRedeem/page.tsx
@@ -203,17 +203,7 @@ export default function Home() {
                         </div>
                         <div style={voucherRightComponent}>
                             <div style={voucherRightCode}>
-                                <div style={voucherCodeLine}>Code: CODE_123sksdiof</div>
-                                <div style={voucherCodeCopy}>
-                                    <svg fill="#000000" width="20px" height="20px" viewBox="0 0 36 36" version="1.1" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" >
-                                        <title>copy-line</title>
-                                        <path d="M29.5,7h-19A1.5,1.5,0,0,0,9,8.5v24A1.5,1.5,0,0,0,10.5,34h19A1.5,1.5,0,0,0,31,32.5V8.5A1.5,1.5,0,0,0,29.5,7ZM29,32H11V9H29Z"></path><path d="M26,3.5A1.5,1.5,0,0,0,24.5,2H5.5A1.5,1.5,0,0,0,4,3.5v24A1.5,1.5,0,0,0,5.5,29H6V4H26Z" ></path>
-                                        <rect x="0" y="0" width="36" height="36" fill-opacity="0" />
-                                    </svg>
-                                    Copy
-                                </div>
                             </div>
-                            <div style={voucherRightStatus}><button type="button" disabled style={buttonVoucherRightStatusActive}>ACTIVE</button></div>
                         </div>
                         <div style={priceButtonDiv}><p style={priceText}>90 Points</p></div>
                         <div style={priceButtonDiv}><link rel="stylesheet" href="" /><button style={buttonRedeem}>Redeem</button></div>
@@ -228,17 +218,7 @@ export default function Home() {
                         </div>
                         <div style={voucherRightComponent}>
                             <div style={voucherRightCode}>
-                                <div style={voucherCodeLine}>Code: CODE_123sksdiof</div>
-                                <div style={voucherCodeCopy}>
-                                    <svg fill="#000000" width="20px" height="20px" viewBox="0 0 36 36" version="1.1" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" >
-                                        <title>copy-line</title>
-                                        <path d="M29.5,7h-19A1.5,1.5,0,0,0,9,8.5v24A1.5,1.5,0,0,0,10.5,34h19A1.5,1.5,0,0,0,31,32.5V8.5A1.5,1.5,0,0,0,29.5,7ZM29,32H11V9H29Z"></path><path d="M26,3.5A1.5,1.5,0,0,0,24.5,2H5.5A1.5,1.5,0,0,0,4,3.5v24A1.5,1.5,0,0,0,5.5,29H6V4H26Z" ></path>
-                                        <rect x="0" y="0" width="36" height="36" fill-opacity="0" />
-                                    </svg>
-                                    Copy
-                                </div>
                             </div>
-                            <div style={voucherRightStatus}><button type="button" disabled style={buttonVoucherRightStatusActive}>ACTIVE</button></div>
                         </div>
                         <div style={priceButtonDiv}><p style={priceText}>90 Points</p></div>
                         <div style={priceButtonDiv}><link rel="stylesheet" href="" /><button style={buttonRedeem}>Redeem</button></div>
@@ -257,17 +237,7 @@ export default function Home() {
                             </div>
                             <div style={voucherRightComponent}>
                                 <div style={voucherRightCode}>
-                                    <div style={voucherCodeLine}>Code: CODE_123sksdiof</div>
-                                    <div style={voucherCodeCopy}>
-                                        <svg fill="#000000" width="20px" height="20px" viewBox="0 0 36 36" version="1.1" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" >
-                                            <title>copy-line</title>
-                                            <path d="M29.5,7h-19A1.5,1.5,0,0,0,9,8.5v24A1.5,1.5,0,0,0,10.5,34h19A1.5,1.5,0,0,0,31,32.5V8.5A1.5,1.5,0,0,0,29.5,7ZM29,32H11V9H29Z"></path><path d="M26,3.5A1.5,1.5,0,0,0,24.5,2H5.5A1.5,1.5,0,0,0,4,3.5v24A1.5,1.5,0,0,0,5.5,29H6V4H26Z" ></path>
-                                            <rect x="0" y="0" width="36" height="36" fill-opacity="0" />
-                                        </svg>
-                                        Copy
-                                    </div>
                                 </div>
-                                <div style={voucherRightStatus}><button type="button" disabled style={buttonVoucherRightStatusNotAvailable}>NOT AVAILABLE</button></div>
                             </div>
                             <div style={priceButtonDiv}><p style={priceText}>90 Points</p></div>
                             <div style={priceButtonDiv}><link rel="stylesheet" href="" /><button style={buttonRedeem}>Redeem</button></div>
@@ -282,17 +252,7 @@ export default function Home() {
                             </div>
                             <div style={voucherRightComponent}>
                                 <div style={voucherRightCode}>
-                                    <div style={voucherCodeLine}>Code: CODE_123sksdiof</div>
-                                    <div style={voucherCodeCopy}>
-                                        <svg fill="#000000" width="20px" height="20px" viewBox="0 0 36 36" version="1.1" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" >
-                                            <title>copy-line</title>
-                                            <path d="M29.5,7h-19A1.5,1.5,0,0,0,9,8.5v24A1.5,1.5,0,0,0,10.5,34h19A1.5,1.5,0,0,0,31,32.5V8.5A1.5,1.5,0,0,0,29.5,7ZM29,32H11V9H29Z"></path><path d="M26,3.5A1.5,1.5,0,0,0,24.5,2H5.5A1.5,1.5,0,0,0,4,3.5v24A1.5,1.5,0,0,0,5.5,29H6V4H26Z" ></path>
-                                            <rect x="0" y="0" width="36" height="36" fill-opacity="0" />
-                                        </svg>
-                                        Copy
-                                    </div>
                                 </div>
-                                <div style={voucherRightStatus}><button type="button" disabled style={buttonVoucherRightStatusNotAvailable}>NOT AVAILABLE</button></div>
                             </div>
                             <div style={priceButtonDiv}><p style={priceText}>90 Points</p></div>
                             <div style={priceButtonDiv}><link rel="stylesheet" href="" /><button style={buttonRedeem}>Redeem</button></div>
