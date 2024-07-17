2066c3270170416215e6c607b93935aa42fcd97a
adriswara <chevellerz454@yahoo.com>
2024-07-17 09:29:20 +0700

    change pass

--- src/app/profile/changePassword/page.tsx
+++ src/app/profile/changePassword/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
 
     return (
         <div className="w-full h-full border-2 border-solid border-jonasBorder rounded-[10px]">
-            changePassword
+            <div>Old Password <input type="text" name="" placeholder="Cari Voucher" className="h-9" />
+            </div>
+            <div>New Password <input type="text" name="" placeholder="Cari Voucher" className="h-9" />
+            </div>
         </div>
     );
 }
