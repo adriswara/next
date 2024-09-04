7e03916ba83ed8d67180fadb4cb294df105fbdbe
adriswara <chevellerz454@yahoo.com>
2024-09-04 10:36:01 +0700

    save state for link

--- src/app/profile/changePassword/page.tsx
+++ src/app/profile/changePassword/page.tsx
@@ -1,9 +1,15 @@
 import PasswordField from "@/components/molecules/PasswordField.molecule";
-
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+const headersList = headers()
+const pathname = headersList.get('x-pathname');
+console.log(pathname)
 export default function Home() {
-
+    
     return (
         <div className="py-6 px-6 w-full h-full border-2 border-solid border-jonasBorder rounded-[10px]">
+                  <div>Referer: {pathname}</div>
+
             <h3 className="text-base font-semibold leading-7 text-gray-900">Change Password</h3>
             <div className="border-gray-100 mt-2">
                 <form className="space-y-2">
--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,14 +1,14 @@
-'use client'
 import ProfileXpBar from "@/components/molecules/ProfileXpBar.molecule";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from 'next/navigation'
+import { headers } from 'next/headers'
 import { Children } from "react";
 import { Poppins } from "next/font/google";
 import ProfileStatText from "@/components/molecules/ProfileStatText.molecule";
 import ProfileName from "@/components/atoms/ProfileName.atom";
 import ProfileLevel from "@/components/atoms/ProfileLevel.atom";
 import GetData from "@/services/getData.service";
+import { redirect } from "next/navigation";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -16,23 +16,34 @@ const poppins = Poppins({
 })
 
 
-export default function profile({
+export default async function profile({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
 
-  var data = (async () => { await GetData("userGet/1") })();
+  const data = await GetData("userGet/1")
   console.log(data)
-  // const data = await GetData("userGet/1")
-  const pathname = usePathname()
-  console.log(pathname)
+
+  const headersList = headers()
+  // const header_url = headersList.get('x-url') || "";
+  const pathname = headersList.get('x-pathname');
+  // const origin_url = headersList.get('x-origin');
+  var profileButtonId = null
+  if (typeof window !== "undefined") {
+    profileButtonId = document.getElementById('myElement')
+  }
+  if(profileButtonId?.onclick){
+    redirect(`/profile/`)
+  }
+
   return (
     <div className={`${poppins.className} flex pt-5 pb-36 container mx-auto items-start gap-5 `}>
       <div className="w-[312px] flex-col flex gap-5 justify-start">
         <div className="border rounded-lg border-solid border-[#e5e7eb] h-auto" >
+          <div>Referer: {pathname}</div>
           <ul>
-            <li className={`link ${pathname === '/profile' ? 'bg-[#e5e7eb]  border-[#e5e7eb] pl-0 pr-0 py-2 px-4 font-semibold' : ' border-[#e5e7eb] pl-0 pr-0 py-2 px-4'}`}><Link href="/profile" className="text-sm pt[8px] pb[5px] pl-[10px] pr-[1rem]">Profile</Link></li>
+            <li id="profile" className={`link ${pathname === '/profile' ? 'bg-[#e5e7eb]  border-[#e5e7eb] pl-0 pr-0 py-2 px-4 font-semibold' : ' border-[#e5e7eb] pl-0 pr-0 py-2 px-4'}`}><Link href="/profile" className="text-sm pt[8px] pb[5px] pl-[10px] pr-[1rem]">Profile</Link></li>
             <li className={`link ${pathname === '/profile/changePassword' ? 'bg-[#e5e7eb] border-t-2 border-[#e5e7eb] pl-0 pr-0 py-2 px-4 font-semibold' : 'border-t-2 border-[#e5e7eb] pl-0 pr-0 py-2 px-4'}`}><Link href="/profile/changePassword" className="text-sm pt[8px] pb[5px] pl-[10px] pr-[1rem]">Change Password</Link></li>
             <li className={`link ${pathname === '/profile/transactionList' ? 'bg-[#e5e7eb] border-t-2 border-[#e5e7eb] pl-0 pr-0 py-2 px-4 font-semibold' : 'border-t-2 border-[#e5e7eb] pl-0 pr-0 py-2 px-4'}`}><Link href="/profile/transactionList" className="text-sm pt[8px] pb[5px] pl-[10px] pr-[1rem]">Transaction</Link></li>
             <li className={`link ${pathname === '/profile/voucherList' ? 'bg-[#e5e7eb] border-t-2 border-[#e5e7eb] pl-0 pr-0 py-2 px-4 font-semibold' : 'border-t-2 border-[#e5e7eb] pl-0 pr-0 py-2 px-4'}`}><Link href="/profile/voucherList" className="text-sm pt[8px] pb[5px] pl-[10px] pr-[1rem]">Voucher List</Link></li>
@@ -43,24 +54,24 @@ export default function profile({
         </div>
         {/* nav profile xp */}
 
-        {/* {data.User.map((data: { id_user: number; name_user: number; password_user: number, email_user: number; phone_user: number; adress_user: string; point_user: number; isDelete_user: number }) => ( */}
+        {data.User.map((data: { id_user: number; name_user: string; password_user: number, email_user: number; phone_user: number; adress_user: string; point_user: number; level_user: number; showcase_user: number; isDelete_user: number }) => (
 
 
-        <div className="border border-[#e5e7eb] rounded-[10px]  w-full relative">
-          <div className="ml-auto mr-auto round-[50%]">
-            <Image className="rounded-[60%] mx-auto my-auto" src="/profilePicSq.jpg" width={128} height={13.84} alt="profilePicture" />
+          <div className="border border-[#e5e7eb] rounded-[10px]  w-full relative">
+            <div className="ml-auto mr-auto round-[50%]">
+              <Image className="rounded-[60%] mx-auto my-auto" src="/profilePicSq.jpg" width={128} height={13.84} alt="profilePicture" />
+            </div>
+            {/* profile Name */}
+            <ProfileName name={data.name_user}></ProfileName>
+            {/* profile level */}
+            <ProfileLevel level={data.level_user}></ProfileLevel>
+            {/* profile xp bar */}
+            <ProfileXpBar percentage={50} xpLeft={0} nextLevel={0}></ProfileXpBar>
+            {/* profile stat text */}
+            <ProfileStatText point={data.point_user} showcase={data.level_user} level={data.level_user}></ProfileStatText>
           </div>
-          {/* profile Name */}
-          <ProfileName name={"Ajon Doe"}></ProfileName>
-          {/* profile level */}
-          <ProfileLevel level={0}></ProfileLevel>
-          {/* profile xp bar */}
-          <ProfileXpBar percentage={50} xpLeft={0} nextLevel={0}></ProfileXpBar>
-          {/* profile stat text */}
-          <ProfileStatText point={0} showcase={0} level={0}></ProfileStatText>
-        </div>
 
-        {/* ))} */}
+        ))}
       </div>
       {children}
     </div>
