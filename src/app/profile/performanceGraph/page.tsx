c9bd0912354a3ff821955fe72368fb3fc1b3176e
adriswara <chevellerz454@yahoo.com>
2024-10-29 14:01:46 +0700

    2 new page

---
+++ src/app/profile/performanceGraph/page.tsx
@@ -0,0 +1,19 @@
+import PasswordField from "@/components/molecules/PasswordField.molecule";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+const headersList = headers()
+const pathname = headersList.get('x-pathname');
+export default function Home() {
+    
+    return (
+        <div className="py-6 px-6 w-full h-full border-2 border-solid border-jonasBorder rounded-[10px]">
+
+            <h3 className="text-base font-semibold leading-7 text-gray-900">Performance Graph</h3>
+            <div className="border-gray-100 mt-2">
+              
+            </div>
+        </div>
+    );
+}
+
+
